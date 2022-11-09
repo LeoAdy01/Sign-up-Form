1a6728c270570cd630616dc31f4afa190787c1d6
Dragomir Leo <leoady01@gmail.com>
2022-11-09 22:51:08 +0200

    second update

--- style.js
+++ style.js
@@ -1,2 +1,16 @@
 const bodyStyle = document.querySelector('body').style;
 bodyStyle.color = 'wheat';
+
+$('input').focus(function(){
+    $(this).parents('.form-group').addClass('focused');
+  });
+  
+  $('input').blur(function(){
+    var inputValue = $(this).val();
+    if ( inputValue == "" ) {
+      $(this).removeClass('filled');
+      $(this).parents('.form-group').removeClass('focused');  
+    } else {
+      $(this).addClass('filled');
+    }
+  })  
